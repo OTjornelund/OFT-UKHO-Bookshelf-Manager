6be9eb08cb3cadd03324599297d35d8985da16d1
Oliver Tjornelund <25643296+OTjornelund@users.noreply.github.com>
2022-11-28 13:01:05 +0000

    Used EF to scaffold data model from existing database.

---
+++ OFT-UKHO-Bookshelf-Manager/OFT-UKHO-Bookshelf-Manager/Models/Rental.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OFT_UKHO_Bookshelf_Manager.Models
+{
+    public partial class Rental
+    {
+        public int Id { get; set; }
+        public int CopyId { get; set; }
+        public int UserId { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public DateTime? EndDateTime { get; set; }
+
+        public virtual Copy Copy { get; set; } = null!;
+        public virtual User User { get; set; } = null!;
+    }
+}
---
+++ OFT-UKHO-Bookshelf-Manager/OFT-UKHO-Bookshelf-Manager/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OFT_UKHO_Bookshelf_Manager.Models
+{
+    public partial class User
+    {
+        public User()
+        {
+            Rentals = new HashSet<Rental>();
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string Surname { get; set; } = null!;
+        public string? Email { get; set; }
+
+        public virtual ICollection<Rental> Rentals { get; set; }
+    }
+}
