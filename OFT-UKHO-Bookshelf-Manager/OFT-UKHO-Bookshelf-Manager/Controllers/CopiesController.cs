390ffd5e20e1ed0f726a9393c508f8f1b107208a
Oliver Tjornelund <25643296+OTjornelund@users.noreply.github.com>
2023-01-12 14:13:11 +0000

    Scaffolded Controllers for Copies, Rentals and Users - (also tested GET endpoint on each)

---
+++ OFT-UKHO-Bookshelf-Manager/OFT-UKHO-Bookshelf-Manager/Controllers/CopiesController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OFT_UKHO_Bookshelf_Manager.DbContexts;
+using OFT_UKHO_Bookshelf_Manager.Models;
+
+namespace OFT_UKHO_Bookshelf_Manager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CopiesController : ControllerBase
+    {
+        private readonly BookLibraryContext _context;
+
+        public CopiesController(BookLibraryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Copies
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Copy>>> GetCopies()
+        {
+            return await _context.Copies.ToListAsync();
+        }
+
+        // GET: api/Copies/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Copy>> GetCopy(int id)
+        {
+            var copy = await _context.Copies.FindAsync(id);
+
+            if (copy == null)
+            {
+                return NotFound();
+            }
+
+            return copy;
+        }
+
+        // PUT: api/Copies/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCopy(int id, Copy copy)
+        {
+            if (id != copy.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(copy).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CopyExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Copies
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Copy>> PostCopy(Copy copy)
+        {
+            _context.Copies.Add(copy);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCopy", new { id = copy.Id }, copy);
+        }
+
+        // DELETE: api/Copies/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCopy(int id)
+        {
+            var copy = await _context.Copies.FindAsync(id);
+            if (copy == null)
+            {
+                return NotFound();
+            }
+
+            _context.Copies.Remove(copy);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CopyExists(int id)
+        {
+            return _context.Copies.Any(e => e.Id == id);
+        }
+    }
+}
