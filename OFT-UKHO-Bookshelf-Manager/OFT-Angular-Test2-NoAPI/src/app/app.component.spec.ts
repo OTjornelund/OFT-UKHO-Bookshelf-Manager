8eacf3ef0302dcc750e26dbb6be3a9bbf33535ce
Oliver Tjornelund <25643296+OTjornelund@users.noreply.github.com>
2023-02-06 20:29:18 +0000

    WIP - Completed Intro and Section 1 of Angular's "Tour of Heroes"

---
+++ OFT-UKHO-Bookshelf-Manager/OFT-Angular-Test2-NoAPI/src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'OFT-Angular-Test2-NoAPI'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('OFT-Angular-Test2-NoAPI');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('OFT-Angular-Test2-NoAPI app is running!');
+  });
+});
