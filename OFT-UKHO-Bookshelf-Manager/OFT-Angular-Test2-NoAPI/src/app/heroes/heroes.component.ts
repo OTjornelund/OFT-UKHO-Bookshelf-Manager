7419aae18df82863597ed7e8af8523810fe78380
Oliver Tjornelund <25643296+OTjornelund@users.noreply.github.com>
2023-02-16 22:43:09 +0000

    Working interim step during chapter 4 of Angular "Tour of Heroes"

--- OFT-UKHO-Bookshelf-Manager/OFT-Angular-Test2-NoAPI/src/app/heroes/heroes.component.ts
+++ OFT-UKHO-Bookshelf-Manager/OFT-Angular-Test2-NoAPI/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+//import { HEROES } from '../mock-heroes'; // <-- No longer needed
+import { HeroService } from '../hero.service';
 
 @Component ({
     selector: 'app-heroes',
@@ -8,9 +9,22 @@ import { HEROES } from '../mock-heroes';
     styleUrls: ['./heroes.component.css']
   })
 export class HeroesComponent {
-  heroes = HEROES;
+
+  heroes: Hero[] = []; //Previously: heroes = HEROES;
 
   selectedHero?: Hero;
+
+  //Parameter below both defines private property and identifies it as "HeroService" injection site.
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes(); //Angular calls ngOnInit() by itself, after the constructor
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes(); //Retrieves heroes from the Service
+    //(This consumes the getHeroes() result as if HeroService can fetch heroes _synchronously_.)
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
